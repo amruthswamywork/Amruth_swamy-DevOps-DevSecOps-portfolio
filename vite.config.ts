4415981469d16b3986f4e4fe4fb53ed58fd747cf
AMRUTH SWAMY C P <amruthswamy11@gmail.com>
2026-06-03 15:28:06 +0530

    new update33

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Amruthswamy_DevOps-DevSecOps_Enginner-Portfolio/",
+  base: "/Amruth_swamy-DevOps-DevSecOps-portfolio/",
 });
